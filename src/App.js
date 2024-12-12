1f1d03438eac92cdd190a231ad44de9f9d86ef60
Abdul jaleel <abduljaleelxwave@gmail.com>
2024-12-12 13:24:43 +0500

    Start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hello brother</h1>
+    </div>
+  );
+}
+
+
+
+export default App;
